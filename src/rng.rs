aad362835fc7ca7b32f388d2258e03669050ec64
Adrian Stein <adrian.stein@tum.de>
2022-03-09 12:10:58 +0100

    Fixed rng (can now produce 255 as well)

--- src/rng.rs
+++ src/rng.rs
@@ -3,19 +3,18 @@ use rand::{prelude::ThreadRng, distributions::Uniform, Rng};
 #[derive(Clone, Debug)]
 pub struct RandomByte {
     rng: ThreadRng,
-    side: Uniform<u8>,
+    distr: Uniform<u16>,
 }
 
 impl RandomByte {
     pub fn new() -> RandomByte {
         RandomByte {
             rng: rand::thread_rng(),
-            // oof, this can't produce 255
-            side: Uniform::new(0, 255),
+            distr: Uniform::new(0, 256),
         }
     }
 
     pub fn sample(&mut self) -> u8 {
-        self.rng.sample(self.side)
+        self.rng.sample(self.distr) as u8
     }
 }
